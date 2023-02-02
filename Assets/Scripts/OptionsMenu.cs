96610f9005d091d3ddcac7761d975fee06958f62
Timo654 <41972888+Timo654@users.noreply.github.com>
2023-02-02 19:41:40 +0200

    add resolution options

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class OptionsMenu : MonoBehaviour
+{
+    Resolution[] resolutions;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new();
+        int currentResolutionIndex = 0;
+        for (int i=0; i < resolutions.Length;i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";
+            options.Add(option);
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
