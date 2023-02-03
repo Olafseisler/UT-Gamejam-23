27154aa317f07498ddbeaa5708828d349b516863
Timo654 <41972888+Timo654@users.noreply.github.com>
2023-02-03 13:39:57 +0200

    change font, add keyboard navigation to menus

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,40 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-    public GameObject pauseMenuUI;
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private GameObject pauseMenuUI;
+    private PlayerControls playerControls;
+    private InputAction menu;
+
+    private void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        playerControls = new PlayerControls();
+    }
+
+
+    private void OnEnable()
+    {
+        menu = playerControls.Menu.Escape;
+        menu.Enable();
+        menu.performed += Pause;
+    }
+
+    private void OnDisable()
+    {
+        menu.Disable();
+    }
+
+
+    public void Pause(InputAction.CallbackContext context)
+    {
+        GameIsPaused = !GameIsPaused;
+        if (GameIsPaused)
         {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
         }
     }
 
-    public void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
-    }
-
-    void Pause()
+    void ActivateMenu()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
-        GameIsPaused = true;
     }
 
+    public void DeactivateMenu()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
     public void LoadMenu()
     {
         Time.timeScale = 1f;
