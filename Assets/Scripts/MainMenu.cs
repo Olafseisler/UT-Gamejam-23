36267255b820a10f9e30535a605062db46a660d3
Timo654 <41972888+Timo654@users.noreply.github.com>
2023-02-03 15:02:21 +0200

    fix checkmark and adjust audio further

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,7 +9,7 @@ public class MainMenu : MonoBehaviour
     void Start()
     {
         AudioManager.SetMusicVolume(PlayerPrefs.GetFloat("MusicVolume", 0.2f));
-        AudioManager.SetSoundVolume(PlayerPrefs.GetFloat("SFXVolume", 0.75f));
+        AudioManager.SetSoundVolume(PlayerPrefs.GetFloat("SFXVolume", 0.5f));
         AudioManager.PlayMusic(Music.menu_music);
     }
 
