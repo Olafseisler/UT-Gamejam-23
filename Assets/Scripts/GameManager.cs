b1c842ee9c79d15150f363e4e31ff1adcb85beb2
Olaf Seisler <olaf.seisler@gmail.com>
2023-02-04 17:43:33 +0200

    Added displaying animal pfps in sacrifice dialog.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -85,4 +85,9 @@ public class GameManager : MonoBehaviour
     
     void HandleWin(){}
     
+    public void SelectSacrifice(GameObject gameObject)
+    {
+        
+    }
+    
 }
--- Assets/Scripts/SacrificeController.cs
+++ Assets/Scripts/SacrificeController.cs
@@ -17,12 +17,16 @@ public class SacrificeController : MonoBehaviour
         // Display the serialized sacrifice objects data in the menu
         for (var i = 0; i < sacrificeSlotsParent.childCount; i++)
         {
-            if (sacrifices.Length < sacrificeSlotsParent.childCount)
+            if (sacrifices.Length <= i)
             {
                 break;
             }
             sacrificeSlotsParent.GetChild(i).Find("NameText").GetComponent<TMP_Text>().text = sacrifices[i].sacrifice_name;
-            
+            sacrificeSlotsParent.GetChild(i).GetComponent<Image>().sprite = sacrifices[i].artwork;
+            sacrificeSlotsParent.GetChild(i).GetComponent<Image>().color = Color.white;
+            sacrificeSlotsParent.GetChild(i).Find("CostText").GetComponent<TMP_Text>().text = $"${sacrifices[i].cost.ToString()}";
         }
+        
+        
     }
 }
