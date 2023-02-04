a9409057a00558be046cf8edc489569706955f53
Timo654 <41972888+Timo654@users.noreply.github.com>
2023-02-04 18:19:44 +0200

    basic intro

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using JSAM;
+
+public class Intro : MonoBehaviour
+{
+    [SerializeField] ParticleSystem gold_ps;
+    [SerializeField] ParticleSystem cash_ps;
+    private void Start()
+    {
+        
+    }
+    void OnIntroEnd()
+    {
+        StartCoroutine(IntroEnd());
+    }
+
+
+    IEnumerator IntroEnd()
+    {
+        gold_ps.Play();
+        cash_ps.Play();
+        AudioManager.PlaySound(Sounds.intro_boom);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(0);
+    }
+}
