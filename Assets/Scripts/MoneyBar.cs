dbd0a4c32c94fd635e7ab48f0a274b8884be41a0
Timo654 <41972888+Timo654@users.noreply.github.com>
2023-02-04 21:21:09 +0200

    moneybar to pausemenutemplate scene

---
+++ Assets/Scripts/MoneyBar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class MoneyBar : MonoBehaviour
+{
+    public int money = 100000;
+    [SerializeField] TMP_Text text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.SetText("" + money);
+    }
+}
