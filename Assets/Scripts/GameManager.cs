94a6e281434a384bef0f7e6dded3ac5deec6fce7
Olaf Seisler <olaf.seisler@gmail.com>
2023-02-04 13:26:45 +0200

    Added walking and idle animations.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public enum GameState
@@ -13,6 +14,10 @@ public enum GameState
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform startPos;
+    [SerializeField] private Transform endPos;
+
     private GameState _currentState;
     
     // Start is called before the first frame update
@@ -30,23 +35,33 @@ public class GameManager : MonoBehaviour
     void OnGameStateChanged(GameState newState)
     {
         _currentState = newState;
-
         switch (_currentState)
         {
             case GameState.Start:
                 //TODO: Setup stuff for starting game
+                Debug.Log($"switched game state to start");
+                player.position = startPos.position;
+                OnGameStateChanged(GameState.Running);
                 break;
             case GameState.Sacrifice:
                 //TODO: Open sacrifice animal dialog
+                Debug.Log($"switched game state to sacrifice");
+
                 break;
             case GameState.Running:
                 //TODO: Handle running animation etc.
+                Debug.Log($"switched game state to running");
+
                 break;
             case GameState.Lose:
                 //TODO: Handle loss
+                Debug.Log($"switched game state to loss");
+
                 break;
             case GameState.Win:
                 //TODO: Handle win
+                        Debug.Log($"switched game state to win");
+
                 break;
             default:
                 Debug.Log("Unknown state passed to state manager");
