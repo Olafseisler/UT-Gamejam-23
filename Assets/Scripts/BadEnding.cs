f1519fa15bb6ceaabd47573600b770dfdc921d61
Timo654 <41972888+Timo654@users.noreply.github.com>
2023-02-05 08:53:12 +0200

    badending skip test but failed

--- Assets/Scripts/BadEnding.cs
+++ Assets/Scripts/BadEnding.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using JSAM;
+using UnityEngine.InputSystem;
 public class BadEnding : MonoBehaviour
 {
+    private bool wasSkipEndingPressed = false;
+    private PlayerControls playerControls;
+    private InputAction menu;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,9 +16,39 @@ public class BadEnding : MonoBehaviour
         //AudioManager.PlayMusic();
         StartCoroutine(SilenceForTheFallen());
     }
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        menu = playerControls.Menu.Escape;
+        menu.Enable();
+        menu.performed += Pause;
+    }
+    private void OnDisable()
+    {
+        menu.Disable();
+    }
+
+    public void Pause(InputAction.CallbackContext context) // we're just going to skip credits when esc is pressed
+    {
+        wasSkipEndingPressed = true;
+        Debug.Log("pressed");
+    }
+
     IEnumerator SilenceForTheFallen()
     {
-        yield return new WaitForSecondsRealtime(6f);
+        for (float timer = 6; timer >= 0; timer -= Time.deltaTime)
+        {
+            if (wasSkipEndingPressed)
+            {
+                yield break;
+            }
+            yield return null;
+        }
+        // TODO - it doesnt get here for some reason
         AudioManager.StopMusic();
         SceneManager.LoadScene("MainMenu");
     }
