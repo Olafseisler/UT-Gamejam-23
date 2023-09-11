80c98d3915dbdcd31494021ecbfd785f8ef94296
Timo654 <41972888+Timo654@users.noreply.github.com>
2023-09-11 18:32:20 +0300

    adjust follower speed to make it less choppy

--- Assets/Scripts/AI/FollowPlayer.cs
+++ Assets/Scripts/AI/FollowPlayer.cs
@@ -29,8 +29,7 @@ public class FollowPlayer : MonoBehaviour
             distance = Vector2.Distance(follower.transform.position, toFollow.transform.position);
             Vector2 direction = toFollow.transform.position - follower.transform.position;
             Vector2 hover = new(follower.transform.position.x, follower.transform.position.y + Mathf.Sin(8f * Time.time));
-            follower.transform.position =
-                Vector2.MoveTowards(follower.transform.position, hover, speed * Time.deltaTime/10);
+            follower.transform.position = Vector2.MoveTowards(follower.transform.position, hover, speed * Time.deltaTime/10);
             if (distance > maxDistFromPlayer)
             {
                 follower.transform.position =
